a4a295a00a7210cc7fc323ad5d19713c645b829a
issa-me-sush <coolsushaan@gmail.com>
2024-04-11 06:14:39 +0530

    test

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { fetchMetadata } from "frames.js/next";
+
+export async function generateMetadata() {
+  return {
+    title: "My Page",
+    other: await fetchMetadata(
+      new URL("/frames",  process.env.VERCEL_URL ? `https://{process.env.VERCEL_URL}` : "http://localhost:3001")
+    ),
+  };
+}
+
+export default function Page() {
+  return <span>My existing page</span>;
+}
