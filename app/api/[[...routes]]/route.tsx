c83eea2f2a4c16e0da5c46403187e8f8f1da515f
issa-me-sush <coolsushaan@gmail.com>
2024-04-11 22:16:03 +0530

    endpoints for frames

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -1,6 +1,7 @@
 /** @jsxImportSource frog/jsx */
 
 import { Button, Frog, TextInput } from 'frog'
+import { formatUnits ,formatEther} from 'viem'
 import { devtools } from 'frog/dev'
 // import { neynar } from 'frog/hubs'
 import { handle } from 'frog/next'
@@ -16,9 +17,12 @@ const app = new Frog({
 // Uncomment to use Edge Runtime
 // export const runtime = 'edge'
 
-app.frame('/', (c) => {
+app.frame('/:hash', async (c) => {
   const { buttonValue, inputText, status } = c
   const fruit = inputText || buttonValue
+  const { hash } = c.req.param();
+  const response = await fetch(`https://sepolia.explorer.mode.network/api/v2/transactions/${hash}`);
+  const trx = await response.json()
   return c.res({
     image: (
       <div
@@ -26,7 +30,7 @@ app.frame('/', (c) => {
           alignItems: 'center',
           background:
             status === 'response'
-              ? 'linear-gradient(to right, #432889, #17101F)'
+              ? 'linear-gradient(to right, #FAFA33, #FAFA33)'
               : 'black',
           backgroundSize: '100% 100%',
           display: 'flex',
@@ -40,7 +44,7 @@ app.frame('/', (c) => {
       >
         <div
           style={{
-            color: 'white',
+            color: 'black',
             fontSize: 60,
             fontStyle: 'normal',
             letterSpacing: '-0.025em',
@@ -51,7 +55,7 @@ app.frame('/', (c) => {
           }}
         >
           {status === 'response'
-            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
+            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!! ${trx.hash}` : ''}`
             : 'Welcome!'}
         </div>
       </div>
@@ -60,7 +64,7 @@ app.frame('/', (c) => {
       <TextInput placeholder="Enter custom fruit..." />,
       <Button value="apples">Apples</Button>,
       <Button value="oranges">Oranges</Button>,
-      <Button value="bananas">Bananas</Button>,
+      <Button value="bananas" >Bananas</Button>,
       status === 'response' && <Button.Reset>Reset</Button.Reset>,
     ],
   })
@@ -69,7 +73,8 @@ app.frame('/trx/:hash', async (c) => {
   const { hash } = c.req.param();
 
   // Fetch transaction details using the provided hash
-  const response = await fetch(`https://sepolia.explorer.mode.network/api/v2/transactions/${hash}`);
+  const response = await fetch(`https://explorer.mode.network/api/v2/transactions/${hash}`);
+
   if (!response.ok) {
     // Return an error message or image
     return c.res({
@@ -77,19 +82,301 @@ app.frame('/trx/:hash', async (c) => {
     });
   }
   const transaction = await response.json();
-
+  console.log(transaction)
+  
   
   return c.res({
     image: (
-      <div style={{ color: 'white', display: 'flex', flexDirection: 'column', fontSize: '20px', padding: '20px', justifyContent: 'center', alignItems: 'center' }}>
-        <div>Transaction Hash: {transaction.hash}</div>
-        <div>Status: {transaction.status}</div>
-        <div>Gas Used: {transaction.gas_used}</div>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100%', background: 'linear-gradient(to right, #FAFA33, #FAFA33)', backgroundSize: '100% 100%' }}>
+      <div style={{ 
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'black',
+            fontSize: '20px',
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: '10px',
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+ }}>Hash: {transaction.hash}</div>
+      <div style={{ 
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'black',
+            fontSize: '20px',
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: '10px',
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+ }}>Status: {transaction.status}</div>
+      <div style={{ 
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'black',
+            fontSize: '20px',
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: '10px',
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+ }}>Block: {transaction.block}</div>
+      <div style={{ 
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'black',
+            fontSize: '20px',
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: '10px',
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+ }}>From: {transaction.from.hash}</div>
+      <div style={{ 
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'black',
+            fontSize: '20px',
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: '10px',
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+ }}>To: {transaction.to.hash}</div>
+      <div style={{ 
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'black',
+            fontSize: '20px',
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: '10px',
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+ }}>Fee: {transaction.fee.value} wei</div>
+      <div style={{ 
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'black',
+            fontSize: '20px',
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: '10px',
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+ }}>Method: {transaction.method}</div>
+      {transaction.value && <div style={{ 
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'black',
+            fontSize: '20px',
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: '10px',
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+ }}>Value: {transaction.value} wei</div>}
+      <div style={{ 
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'black',
+            fontSize: '20px',
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: '10px',
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+ }}>Confirmations: {transaction.confirmations}</div>
   
+    </div>
+    ),
+    intents: [
+      <Button.Link href= {` https://explorer.mode.network/tx/${hash}`} >Explorer</Button.Link>,
+
+      
+    ],
+   
+  });
+});
+
+app.frame('/account/:address', async (c) => {
+  const { address } = c.req.param();
+
+  // Fetch Ethereum balance and transaction counts (using placeholder URLs)
+  const balanceResponse = await fetch(`https://explorer.mode.network/api?module=account&action=balance&address=${address}`);
+  console.log("balance response fetched")
+  const txResponse = await fetch(`https://explorer.mode.network/api?module=account&action=txlist&address=${address}`);
+  const tokenTxResponse = await fetch(`https://explorer.mode.network/api?module=account&action=tokentx&address=${address}`);
+  // const tokenNFTTxResponse = await fetch(`https://explorer.mode.network/api?module=account&action=tokennfttx&address=${address}`);
+
+  // Parse the responses
+  const balanceData = await balanceResponse.json();
+  const txData = await txResponse.json();
+  const tokenTxData = await tokenTxResponse.json();
+  // const tokenNFTTxData = await tokenNFTTxResponse.json();
+console.log("all fetched")
+  // Compute data
+  const ethBalance = balanceData.result || '0';
+  const txCount = txData.result.length || 0;
+  const tokenTransferCount = tokenTxData.result.length || 0;
+  // const erc721Count = tokenNFTTxData.result.length || 0;
+
+  return c.res({
+    image: (
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%',
+        background: 'linear-gradient(to right, #FAFA33, #FAFA33)',
+        backgroundSize: '100% 100%',
+      }}>
+        <div style={{ 
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'black',
+            fontSize: '40px',
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: '10px',
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+ }}>
+          {`ETH Balance: ${formatEther(ethBalance)}`}
+        </div>
+        <div style={{ 
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'black',
+            fontSize: '40px',
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: '10px',
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+ }}>
+          {`Total Transactions: ${txCount}`}
+        </div>
+        <div style={{ 
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'black',
+            fontSize: '40px',
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: '10px',
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+ }}>
+          {`Token Transfers: ${tokenTransferCount}`}
+        </div>
+        <div style={{ 
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'black',
+            fontSize: '40px',
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: '10px',
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+ }}>
+          {/* {`ERC-721 Transfers: ${erc721Count}`} */}
+        </div>
       </div>
     ),
-   
-   
+    intents: [
+      <Button.Link href={`https://explorer.mode.network/address/${address}`}>Explorer</Button.Link>,
+      <Button action={`/account/${address}/otherTokens`}>Other Token Balances</Button>,
+    ],
+  });
+});
+
+app.frame('/account/:address/otherTokens', async (c) => {
+  const { address } = c.req.param();
+
+  // Fetch top 5 tokens
+  const tokenListResponse = await fetch(`https://explorer.mode.network/api?module=account&action=tokenlist&address=${address}`);
+  const tokenListData = await tokenListResponse.json();
+  const topTokens = tokenListData.result.slice(0, 10);
+
+  return c.res({
+    image: (
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%',
+        background: 'linear-gradient(to right, #FAFA33, #FAFA33)',
+        backgroundSize: '100% 100%',
+      }}>
+        {/* @ts-ignore  */}
+        {topTokens.map((token, index) => (
+          <div key={index} style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'black',
+            fontSize: '20px',
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: '10px',
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}>
+            {`${token.name} (${token.symbol}): ${
+  token.type === 'ERC-20' ? formatUnits(token.balance, token.decimals) : token.balance
+}`}
+          </div>
+        ))}
+      </div>
+    ),
+    intents:[
+      <Button action={`/account/${address}`}>Back</Button>,
+]
   });
 });
 devtools(app, { serveStatic })
